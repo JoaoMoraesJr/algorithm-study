cf4309cfce74d281fd3206fea2e269c92e256778
Moraes <joao_moraes@dell.com>
2022-12-28 19:38:30 -0300

    Remove stones fixes and priority queue solution

--- General/removeStones.js
+++ General/removeStones.js
@@ -58,7 +58,7 @@ var minStoneSumMergeSort = function(piles, k) {
 };
 
 //Heap
-//Time complexity = O (n log n + k*log n) -> log n for insertion since insertion will not go through all array
+//Time complexity = O (n log n + k log n) -> n log n for insertion on heap + k log n for deleting
 //Space complexity = O (n) 
 var minStoneSum = function(piles, k) {
     class Heap {
@@ -117,9 +117,9 @@ var minStoneSum = function(piles, k) {
 
         bubbleUp() {
             let index = this.heap.length - 1;
-            let parent = this.parent(index);
-            while (parent >= 0 && this.heap[index] > this.heap[parent]) {
-                this.swap(parent, index);
+            while (this.parent(index) >= 0 && this.heap[index] > this.heap[this.parent(index)]) {
+                this.swap(this.parent(index), index);
+                index = this.parent(index);
             }
         }
 
@@ -138,5 +138,26 @@ var minStoneSum = function(piles, k) {
     return heap.sum();
 };
 
+//Priority Queue
+//Time complexity = O (n log n + k log n) -> n log n for insertion on queue + k log n for deleting
+//Space complexity = O (n)
+//Requires additional imports for MaxPriorityQueue
+// var minStoneSum = function(piles, k) {
+//     const queue = new MaxPriorityQueue();
+//     piles.forEach(pile => {
+//         queue.enqueue(pile);
+//     })
+//     for (let i = 0; i < k; i++) {
+//         let halfPile = Math.ceil(queue.dequeue().element/2);
+//         queue.enqueue(halfPile);
+//     }
+//     let result = 0;
+//     while (queue.size()) {
+//         result += queue.dequeue().element;
+//     }
+//     return result;
+// };
+
 console.log(minStoneSum([5, 4, 9], 2));
 console.log(minStoneSum([4, 3, 6, 7], 3));
+console.log(minStoneSum([8011,8387,6007,1235,5595,138,3136,1740,963,9412,802,4475,9695,3713,1742,8559,2237,4356,4012,3449,990,6930,523,931,5937,5902,2817,4088,385,1359,1888,1106,416,670,347,6113,4190,2094,2575,3011,8571,32,6318,9658,708,4061,2481,595,69], 93));
