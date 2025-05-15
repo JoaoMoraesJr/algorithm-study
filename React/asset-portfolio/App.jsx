6a7327aaecacd9bcb133c15eb733c4c035188eb0
JoaoMoraesJr <joaojr1997@gmail.com>
2025-05-15 20:53:10 -0300

    Add React challenge

---
+++ React/asset-portfolio/App.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import './App.css'
+
+function App() {
+    
+    const [data, setData] = useState(null);
+    const [error, setError] = useState('');
+    const [wallet, setWallet] = useState({})
+    
+    function handleAddToWallet(key, value) {
+        var editedWallet = {...wallet};
+        if (!editedWallet[key]){
+            editedWallet[key] = 0;
+        }
+        editedWallet[key] += value.usd;
+        setWallet(editedWallet);
+        console.log(wallet);
+    }
+    
+    function handleRemoveFromWallet(key, value) {
+        var editedWallet = {...wallet};
+        delete editedWallet[key]
+        setWallet(editedWallet);
+        console.log(wallet);
+    }
+    
+    useEffect(() => {
+        fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd')
+        .then(response => {
+            // if (!response.ok) {
+            //     throw Error("Error")
+            // }
+            // return response.json()
+            
+            var responseMock = `{
+                "bitcoin": {
+                    "usd": 103738
+                },
+                "ethereum": {
+                    "usd": 2551.89
+                }
+            }`
+            return JSON.parse(responseMock)
+        })
+        .then(data => setData(data))
+        .catch(error => setError(error.message))
+    }, [])
+    if (error) return <div>{error}</div>
+    if (!data) return <div>Loading...</div>
+    return (
+        <div>
+            <h1>Assets</h1>
+            {Object.entries(data).map(([key, value]) => (
+                <div key={key} className="asset-card">
+                    <div className="asset-text">{key.charAt(0).toUpperCase() + key.slice(1)}: {value.usd}</div>
+                    <button style={{marginLeft: 5}} onClick={() => handleAddToWallet(key, value)}>+</button>
+                </div>
+            ))}
+            <h1>Wallet</h1>
+            {Object.entries(wallet).map(([key, value]) => (
+              <div key={key} className="asset-card">
+                    <div className="asset-text">{key.charAt(0).toUpperCase() + key.slice(1)}: {value}</div>
+                    <button style={{marginLeft: 5}} onClick={() => handleRemoveFromWallet(key, value)}>-</button>
+                </div> 
+            ))}
+        </div>
+    )
+}
+export default App;
